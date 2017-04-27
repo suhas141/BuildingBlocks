e4d320272959cf3f65a3ccf78c9560f04bc97601
suhas141 <murthy.su@husky.neu.edu>
2017-04-27 17:37:02 -0400

    made UI changes and some new build

---
+++ BuildingBlocks/Assets/Scripts/Quitfun.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Quitfun : MonoBehaviour {
+
+	public void IQuit()
+    {
+        Application.Quit();
+
+    }
+}
---
+++ BuildingBlocks/Assets/Touch.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch : MonoBehaviour {
+
+    public GameObject point;
+    private void OnTriggerEnter()
+    {
+        point.SetActive(true);
+    }
+}
